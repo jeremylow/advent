d2e7d1861997167247a31ab8bd80e4162ca65223
Jeremy Low <jeremylow@gmail.com>
2021-12-02 18:52:34 -0500

    day 01

---
+++ day01-01.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+
+def main():
+    with open("data-day-1.txt") as f:
+        data = f.readlines()
+    c = Counter()
+    for idx, line in enumerate(data):
+        try:
+            old = int(data[idx - 1].strip())
+            new = int(line.strip())
+            if old < new:
+                print(old, new)
+                c["up"] += 1
+        except:
+            pass
+    return c
+
+
+if __name__ == "__main__":
+    c = main()
+    print(c)
